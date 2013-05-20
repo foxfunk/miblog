6512325c94fd29186664f7907567b56a03fbe50f
foxfunk <foxheyer@hotmail.com>
2013-05-19 23:53:36 -0400

    este es mi blog en rails

---
+++ mipagina/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mipagina::Application.config.secret_token = 'e6ff9e00b59ae21a691785fc719d1594b5d0367eb8c2d494904f6a4f6c4279b306f012b2a65f5baa014ee27ab42f133f424d5bdd6016f1a5a304fe18b89aec77'
